6843a17d25322cd6493ca5482d64db9f2999cabe
JatinSarwa10 <sarwajatin@gmail.com>
2026-01-01 18:43:12 +0530

    feat: Create AppShell layout wrapper for page structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,35 @@
 import { Route, Routes } from 'react-router-dom'
 import './index.css'
 import Home from './pages/Home'
+import { RedirectToSignIn, SignedIn, SignedOut } from '@clerk/clerk-react'
+import AppShell from './components/AppShell'
+import Dashboard from './pages/Dashboard'
 
-function App() {
-  
+const ClerkProtected = ({children}) => (
 
+  <>
+  <SignedIn>{children}</SignedIn>
+  <SignedOut>
+    <RedirectToSignIn />
+  </SignedOut>
+  </>
+)
+
+
+const App = () =>{
   return (
-   <Routes>
+  <div className='min-h-screen max-w-full overflow-x-hidden'>
+     <Routes>
     <Route path = '/' element= {<Home/>}/>
+    <Route path='/app' element={<ClerkProtected>
+      <AppShell />
+    </ClerkProtected>} >
+    <Route index element={<Dashboard />}  />
+    <Route path="dashboard" element={<Dashboard />} />
+    </Route>
    </Routes>
+
+  </div>
 
   )
 }
